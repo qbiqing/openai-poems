03e99d26f2788bf279a5e4192406b7f83db46c57
qbiqing <bq2134@columbia.edu>
2021-05-30 12:52:17 +0800

    remove unused

--- src/App.js
+++ src/App.js
@@ -24,18 +24,6 @@ function App() {
       <header className="App-header">
         <Example />
         <PromptForm />
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
     </div>
   );

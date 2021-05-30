7052d226f9fa5c563cca420940b9c37dce31bbb9
qbiqing <bq2134@columbia.edu>
2021-05-30 12:33:50 +0800

    Returns poem

--- src/form.js
+++ src/form.js
@@ -14,29 +14,49 @@ class PromptForm extends React.Component {
     }
   
     handleSubmit(event) {
-    fetch('https://api.openai.com/v1/engines/davinci/completions', {
+      fetch('https://api.openai.com/v1/engines/davinci/completions', {
         method: 'POST',
         headers: {
             Accept: 'application/json',
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer <INSERT YOUR API KEY>'
         },
         body: JSON.stringify({
-            prompt: this.state.value,
-            max_tokens: 26
+            prompt: 'Poem about ' + this.state.value + ':\n',
+            max_tokens: 60,
+            temperature: 0.6,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0.3,
+            stop: ['###']
         })
-    });
+      }).then(response => response.json())
+        .then(data => this.setState({data: data}));
+
       event.preventDefault();
     }
   
     render() {
       return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Enter prompt of one or more words: <br/>
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+        <div className="form-and-poem">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Enter prompt of one or more words: <br/>
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          <br/>
+          <div className="poem">
+            {this.state.data &&
+              <div>
+                  {this.state.data.choices[0].text.split('\n').map((i,key) => {
+                      return <div key={key}>{i}</div>;
+                  })}
+              </div>
+            }
+          </div>
+        </div>
       );
     }
   }
